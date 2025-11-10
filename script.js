5ff6883fdaf432623a270728d6b1e0956dd46098
Deksiyos <deksiman721@gmail.com>
2025-11-10 23:07:27 +0300

    refactor function in script.js

--- script.js
+++ script.js
@@ -11,27 +11,99 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(function(position){
-    
-    const{latitude,longitude}=position.coords;
-   
 
+class Workout{
+    date=new Date();
+    id= Date.now() + ''.slice(-10);
+    constructor(coords,distance,duration){
+        // this.date=...
+        // thbis.id=...
+        this.coords=coords;
+        this.distance=distance;
+        this.duration=duration;
+    }
+}
+class Running extends Workout{
+     constructor(coords,distance,duration,cadence){
+        super(coords,distance,duration);
+        this.cadence=cadence;
+        this.calcPace();
+     }
+     calcPace(){
+        this.pace=this.duration/this.distance;
+        return this.pace
+     }
+}
+class Cycling extends Workout{
+      constructor(coords,distance,duration,elevationonGain){
+        super(coords,distance,duration);
+        this.elevationonGain=elevationonGain;
+        this.calcSpeed();
+     }
+     calcSpeed(){
+        this.speed= this.distance/(this.duration/60);
+        return this.speed;
+     }
+}
+const run1=new Running([39,-12],5.2,24,178);
+const cycling1=new Cycling([39,-12],27,95,523);
+console.log(run1,cycling1)
+
+////////////////////////////
+// APPLICATION ARCHITECTURE
+class App{
+    #map;
+    #mapEvent;
+    constructor(){
+
+        this._getPosition();
+
+        form.addEventListener('submit',this._newWorkout.bind(this)
+   
+)
+
+  inputType.addEventListener('change',this._toggleElevationField)
+    }
+    _getPosition(){
+        if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(this._loadMap. bind(this),
+    function(){
+    alert('could not get your location');
+})
+    }
+    _loadMap(position){
+    const{latitude,longitude}=position.coords;
     const coords=[latitude,longitude];
 
-    const map = L.map('map').setView(coords, 13);
+     this.#map = L.map('map').setView(coords, 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(this.#map);
+ // Handling click on map
+   this.#map.on('click',this._showForm.bind(this));
+    }
+    _showForm(mapE){
+        this.#mapEvent=mapE
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+    
+    _toggleElevationField(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e){
+        e.preventDefault();
+   inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
 
 
+    // Display marker
+      
+        const {lat,lng}=this.#mapEvent.latlng;
 
-    map.on('click',function(mapEvent){
-        console.log(mapEvent);
-        const {lat,lng}=mapEvent.latlng;
-
-         L.marker([lat,lng]).addTo(map)
+         L.marker([lat,lng]).addTo(this.#map)
     .bindPopup(L.popup({
         maxWidth:250,
         minWidth:100,
@@ -40,10 +112,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }))
     .setPopupContent('Working')
     .openPopup();
-    });
+    }
+}
 
-   
+const app=  new App();
 
-},function(){
-    alert('could not get your location');
-})
+
