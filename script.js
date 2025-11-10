3c2a3fe4a396ffb85bbd76dc3e4404c4676287bf
Deksiyos <deksiman721@gmail.com>
2025-11-10 19:57:26 +0300

    add map

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function(position){
+    
+    const{latitude,longitude}=position.coords;
+   
+
+    const coords=[latitude,longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+    map.on('click',function(mapEvent){
+        console.log(mapEvent);
+        const {lat,lng}=mapEvent.latlng;
+
+         L.marker([lat,lng]).addTo(map)
+    .bindPopup(L.popup({
+        maxWidth:250,
+        minWidth:100,
+        autoClose:false,
+        closeOnClick:false,className:'running-popup' 
+    }))
+    .setPopupContent('Working')
+    .openPopup();
+    });
+
+   
+
+},function(){
+    alert('could not get your location');
+})
